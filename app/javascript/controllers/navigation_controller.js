726488ebed97226104232ded0b7b1d8867046926
Nishan Tamang <n.tamang@paicon.com>
2025-06-01 17:32:18 +0200

    add navigation with buttons on blocks implementation

---
+++ app/javascript/controllers/navigation_controller.js
@@ -0,0 +1,49 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="navigation"
+export default class extends Controller {
+  static targets = [ "field" ];
+
+  up(event) {
+    this.#navigateTo(event, -1);
+  }
+
+  down(event) {
+    this.#navigateTo(event, 1);
+  }
+
+  left(event) {
+    const { target } = event;
+
+    if (target.selectionStart !== 0) return;
+
+    this.#navigateTo(event, -1);
+  }
+
+  right(event) {
+    const { target } = event;
+
+    if (target.selectionEnd !== target.value.length) return;
+
+    this.#navigateTo(event, 1, 0);
+  }
+
+  // private
+
+  #navigateTo(event, direction, position) {
+    const currentIndex = this.fieldTargets.indexOf(event.target);
+    const newIndex = currentIndex + direction;
+
+    if (newIndex < 0 || newIndex >= this.fieldTargets.length) return;
+
+    event.preventDefault();
+
+    const field = this.fieldTargets[newIndex];
+
+    field.focus();
+
+    const cursorPosition = position ?? field.value.length;
+
+    field.setSelectionRange(cursorPosition, cursorPosition);
+  }
+}
